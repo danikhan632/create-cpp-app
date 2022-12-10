32737d14e6b6b46de4caa76a6de91b3816f9bd8f
danikhan632 <danikhan632@gmail.com>
2022-12-10 15:56:01 -0500

    47 commit

--- build_sys/installers.py
+++ build_sys/installers.py
@@ -43,5 +43,5 @@ def installing_deps(data):
 
 
 def system_config(data):
-    build_info = "The following info has been detected:\r\nOperating System: "+platform.system()+"\r\nbrew installled: "+isInstalled("brew")+"\r\npip installed: "+isInstalled("brew")+"\r\nArchitecture: "+platform.architecture()[0] +"\r\nMachine: " + platform.machine()+ "\r\nNode: " + platform.node()+"\r\nSystem: " + platform.system()
+    build_info = "The following info has been detected:\r\nOperating System: "+platform.system()+"\r\nbrew installled: "+str(isInstalled("brew"))+"\r\npip installed: "+str(isInstalled("brew"))+"\r\nArchitecture: "+platform.architecture()[0] +"\r\nMachine: " + platform.machine()+ "\r\nNode: " + platform.node()+"\r\nSystem: " + platform.system()
     return build_info
