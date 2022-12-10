f170549f5265eae12e847bd1e52626cb2e68b56f
danikhan632 <danikhan632@gmail.com>
2022-12-10 18:24:47 -0500

    54 commit

--- build_sys/bootstrap.py
+++ build_sys/bootstrap.py
@@ -8,7 +8,7 @@ import sys
 from .change_name import change_proj_name
 from distutils.spawn import find_executable
 from .docker import update_docker_files
-from .installers import system_config,installing_deps
+from .installers import system_config,installing_deps,install_code_addons
 
 
 def isInstalled(pkg):
@@ -28,55 +28,12 @@ def bootstrap(data):
     install_code_addons(data)
     change_proj_name(data)
     update_docker_files("myproj",data)
-    data["bootstraped"]=True
 
-    json.dump(data, open(".config/data.json", "w"), indent = 4)
     os.system("curl https://hackgtstoragebucket.s3.amazonaws.com/cpx.json > ~/.cpx.json")
     os.system("python3 scripts.py dev")
 
 
-def openCode():
-    cont= False
-    vscode=""
-    print(cont)
-    if cont:
-        vscode=str(input("open vs_code? y or n:  "))
-    while cont:
-            if vscode == "y":
-                sleep(1)
-                os.system("code .")
-            elif vscode == "n":
-                cont=False
-            else:
-                os.sys("pwd")
 
-def install_code_addons(data):
-    cont= not is_cpx_boot()
-    vscode=""
-    print(cont)
-    if cont:
-        vscode=str(input("install vscode packages? y or n:  "))
-    
-
-    while cont:
-            if vscode == "y":
-                os.system("code --install-extension ms-vscode.cpptools-extension-pack")
-                os.system("code --install-extension twxs.cmake")
-                os.system("code --install-extension FireBlackHat.conan-tools")
-                os.system("code --install-extension disroop.conan")
-                os.system("code --install-extension DamianKoper.gdb-debug")
-                os.system("code --install-extension matepek.vscode-catch2-test-adapter")
-                os.system("code --install-extension ms-vscode.cmake-tools")
-                os.system("code --install-extension ms-vscode.makefile-tools")
-                os.system("code --install-extension hbenl.vscode-test-explorer")
-                cont=False
-                data["bootstraped"]=True
-                json.dump(data, open(".config/data.json", "w"), indent = 4)
-            elif vscode == "n":
-                cont=False
-            else:
-                print("please make a valid choice")
-                vscode=str(input("install vscode packages? y or n:  "))
 
 
 
--- build_sys/installers.py
+++ build_sys/installers.py
@@ -25,25 +25,11 @@ def installing_deps(data):
             print("please make a valid choice: ")
             slec = str(input("\r\nis this correct? (y or n): "))
 
-    if "darwin" in platform.system().lower():
-        print("mac detected")
-        os.system("xcode-select --install")
-        data["os"]="mac"
-        data["gcced"]=True
-        json.dump(data, open(".config/data.json", "w"), indent = 4)
+    man = listPackageManagers()
+    print(man)
+    selectPackageManager(man)
 
 
-    print(listPackageManagers())
-
-    if not isInstalled("brew"): 
-        os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
-        os.system("brew install conan")
-        os.system("brew install cmake")
-        os.system("brew install clang")
-        data["brewed"]=True
-        json.dump(data, open(".config/data.json", "w"), indent = 4)
-        data["piped"]=True
-        json.dump(data, open(".config/data.json", "w"), indent = 4)
 
 
 
@@ -70,10 +56,109 @@ def listPackageManagers():
     if isInstalled("dnf"):
         count+=1
         managers.append("dnf")
-    if isInstalled("yum"):
-        count+=1
-        managers.append("yum")
     if isInstalled("rpm"):
         count+=1
         managers.append("rpm")
     return managers
+
+def selectPackageManager(managers):
+    if "darwin" in platform.system().lower():
+        print("mac detected")
+        os.system("xcode-select --install")    
+        if not isInstalled("brew"): 
+            os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
+        brewInstall()
+        return
+
+    ok = False
+    for i in range(0, len(managers)):
+        print("\r\n "+i+" "+ managers[i])
+    slec = int(input("select package manager to use: "))
+    while not ok:
+        if slec in range(0, len(managers)):
+            ok=True
+        else:
+            print("please make a valid choice: ")
+            int(input("select package manager to use: "))
+
+    if managers[slec] == "brew":
+        brewInstall()
+    elif managers[slec] == "pip":
+        pipInstall()
+    elif managers[slec] == "apt":
+        aptInstall()
+    elif managers[slec] == "dnf":
+        dnfInstall()
+    else:
+        print("no package manager detected")
+    
+def brewInstall():
+    os.system("brew install conan")
+    os.system("brew install cmake")
+    os.system("brew install clang")
+    if isInstalled("apt"):
+         os.system("sudo apt-get install -y build-essential")
+    elif isInstalled("dnf"):
+        os.system("sudo dnf install make automake gcc gcc-c++ kernel-devel")
+
+def pipInstall():
+    os.system("sudo pip install conan")
+    os.system("sudo pip install cmake")
+    os.system("sudo pip install clang")
+    if isInstalled("apt"):
+        os.system("sudo apt-get install -y build-essential")
+    elif isInstalled("dnf"):
+        os.system("sudo dnf install make automake gcc gcc-c++ kernel-devel")
+
+def aptInstall():
+    os.system("sudo apt-get install -y build-essential")
+    os.system("sudo apt-get install -y conan")
+    os.system("sudo apt-get install -y cmake")
+    os.system("sudo apt-get install -y clang")
+def dnfInstall():
+    os.system("sudo dnf install cmake")
+    os.system("sudo dnf install conan")
+    os.system("sudo dnf install make automake gcc gcc-c++ kernel-devel")
+
+
+
+def install_code_addons(data):
+    cont= True
+    vscode=""
+    if cont:
+        vscode=str(input("install vscode packages? y or n:  "))
+    
+
+    while cont:
+            if vscode == "y":
+                os.system("code --install-extension ms-vscode.cpptools-extension-pack")
+                os.system("code --install-extension twxs.cmake")
+                os.system("code --install-extension FireBlackHat.conan-tools")
+                os.system("code --install-extension disroop.conan")
+                os.system("code --install-extension DamianKoper.gdb-debug")
+                os.system("code --install-extension matepek.vscode-catch2-test-adapter")
+                os.system("code --install-extension ms-vscode.cmake-tools")
+                os.system("code --install-extension ms-vscode.makefile-tools")
+                os.system("code --install-extension hbenl.vscode-test-explorer")
+                cont=False
+                data["bootstraped"]=True
+                json.dump(data, open(".config/data.json", "w"), indent = 4)
+            elif vscode == "n":
+                cont=False
+            else:
+                print("please make a valid choice")
+                vscode=str(input("install vscode packages? y or n:  "))
+def openCode():
+    cont= False
+    vscode=""
+    print(cont)
+    if cont:
+        vscode=str(input("open vs_code? y or n:  "))
+    while cont:
+            if vscode == "y":
+                sleep(1)
+                os.system("code .")
+            elif vscode == "n":
+                cont=False
+            else:
+                os.sys("pwd")
