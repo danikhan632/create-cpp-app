300d3cbe80f790c2ebdf4f68307328771cced23c
danikhan632 <danikhan632@gmail.com>
2022-12-10 14:32:46 -0500

    43 commit

--- build_sys/docker.py
+++ build_sys/docker.py
@@ -8,13 +8,13 @@ import sys
 
 def update_docker_files(old_name,new_name):
     cmk=None
-    with open('./docker_files/Dockerfile.build','r',encoding='utf-8') as file:
+    with open('./build_sys/docker_files/Dockerfile.build','r',encoding='utf-8') as file:
         cmk = file.readlines()
     for i in range(0, len(cmk)):
         if old_name in cmk[i]:
             cmk[i]= cmk[i].replace(old_name, new_name)
 
-    with open('./docker_files/Dockerfile.build', 'w', encoding='utf-8') as file:
+    with open('./build_sys/docker_files/Dockerfile.build', 'w', encoding='utf-8') as file:
         file.writelines(cmk)
 
 def docker_build(proj_name):
