07eb0f48f340974695c335923e90068f327a0d55
danikhan632 <danikhan632@gmail.com>
2022-12-10 17:34:10 -0500

    49 commit

--- build_sys/installers.py
+++ build_sys/installers.py
@@ -13,6 +13,27 @@ def isInstalled(pkg):
 
 def installing_deps(data):
     print(system_config(data))
+    ok = False
+    slec = str(input("\r\nis this correct? (y or n): "))
+    while not ok:
+        if "y" in slec.lower():
+            ok=True
+        elif "n" in slec.lower():
+            print("aborting")
+            exit()
+        else:
+            print("please make a valid choice: ")
+            slec = str(input("\r\nis this correct? (y or n): "))
+
+    if "darwin" in platform.system().lower():
+        print("mac detected")
+        os.system("xcode-select --install")
+        data["os"]="mac"
+        data["gcced"]=True
+        json.dump(data, open(".config/data.json", "w"), indent = 4)
+
+
+    print(listPackageManagers())
 
     if not isInstalled("brew"): 
         os.system("/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\"")
@@ -25,24 +46,34 @@ def installing_deps(data):
         json.dump(data, open(".config/data.json", "w"), indent = 4)
 
 
-        if "darwin" in platform.system().lower():
-            os.system("xcode-select --install")
-            data["os"]="mac"
-            data["gcced"]=True
-            json.dump(data, open(".config/data.json", "w"), indent = 4)
-
-        else:
-
-            # distribution
-            dist = platform.dist()
-            dist = " ".join(x for x in dist)
-            print("Distribution: " + dist)
-            data["os"]="arch"
-            data["gcced"]=True
-            json.dump(data, open(".config/data.json", "w"), indent = 4)
 
 
 
 def system_config(data):
-    build_info = "\n\n\nThe following info has been detected:\r\nOperating System: "+platform.system()+"\r\nbrew installled: "+str(isInstalled("brew"))+"\r\npip installed: "+str(isInstalled("brew"))+"\r\nArchitecture: "+platform.architecture()[0] +"\r\nMachine: " + platform.machine()+ "\r\nNode: " + platform.node()+"\r\n apt: "+str(isInstalled("brew")) +"\r\n rpm: "+str(isInstalled("rpm")) +"\r\n yum: "+str(isInstalled("yum")) +"\r\n rpm: "+str(isInstalled("yum"))+"\r\n is this correct(y or n)?: "
+    build_info = "\n\n\nThe following info has been detected:\r\nOperating System: "+platform.system()+"\r\nbrew installled: "+str(isInstalled("brew"))+"\r\npip installed: "+str(isInstalled("brew"))+"\r\nArchitecture: "+platform.architecture()[0] +"\r\nMachine: " + platform.machine()+ "\r\nNode: " + platform.node()+"\r\n apt: "+str(isInstalled("brew")) +"\r\n rpm: "+str(isInstalled("rpm")) +"\r\n yum: "+str(isInstalled("yum")) +"\r\n yum: "+str(isInstalled("yum"))
     return build_info
+
+
+
+def listPackageManagers():
+    managers=[]
+    count=0
+    if isInstalled("brew"):
+        count+=1
+        managers.append("brew")
+    if isInstalled("pip"):
+        count+=1
+        managers.append("pip")
+    if isInstalled("apt"):
+        count+=1
+        managers.append("apt")
+    if isInstalled("dnf"):
+        count+=1
+        managers.append("dnf")
+    if isInstalled("yum"):
+        count+=1
+        managers.append("yum")
+    if isInstalled("rpm"):
+        count+=1
+        managers.append("rpm")
+    return managers
