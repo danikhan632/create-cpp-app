fa4294815cd71bf371ca0e20a29f2f098e034ec1
danikhan632 <danikhan632@gmail.com>
2022-12-29 10:35:21 -0500

    110 commit

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-\nalias cpx=\"python3 scripts.py\">> ~/.zshrc
+"\nalias cpx=\"python3 scripts.py\"">> ~/.zshrc
 
-\nalias create-cpp-app=\"python3 ~/.create.py\" >> ~/.zshrc
-\nalias cpx=\"python3 scripts.py\">> ~/.bashrc
+"\nalias create-cpp-app=\"python3 ~/.create.py\"" >> ~/.zshrc
+"\nalias cpx=\"python3 scripts.py\"">> ~/.bashrc
 
-\nalias create-cpp-app=\"python3 ~/.create.py\" >> ~/.bashrc
+"\nalias create-cpp-app=\"python3 ~/.create.py\"" >> ~/.bashrc
 
-source ~/.zshrc
-source ~/.bashrc
+# source ~/.zshrc
+# source ~/.bashrc
 
 
