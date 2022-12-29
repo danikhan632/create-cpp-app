aa8d2d4e0e7fb80acbf3f7181f22a6c19f59efdf
danikhan632 <danikhan632@gmail.com>
2022-12-29 10:29:13 -0500

    109 commit

---
+++ .create.py
@@ -0,0 +1,11 @@
+# sys.argv[1]
+import os
+import sys
+def main():
+    os.system("git clone https://github.com/danikhan632/create-cpp-app.git")
+    os.system(" mv ./create-cpp-app ./", sys.argv[1])
+    os.system("cd ", sys.argv[1])
+    os.system("python3 scripts.py bootstrap ", sys.argv[1])    
+    
+if __name__ == "__main__":
+   main()
--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
 
-function create-cpp-app(){
-    git clone https://github.com/danikhan632/create-cpp-app.git
-    mv ./create-cpp-app ./$1
-    cd $1
-    python3 scripts.py bootstrap $1
-}
-alias cpx="python3 scripts.py"
-#/bin.bash while IFS= read -r line do echo "$line" echo -e "$line
-" >>~/zsh.txt done <"install.sh"
+\nalias cpx=\"python3 scripts.py\">> ~/.zshrc
+
+\nalias create-cpp-app=\"python3 ~/.create.py\" >> ~/.zshrc
+\nalias cpx=\"python3 scripts.py\">> ~/.bashrc
+
+\nalias create-cpp-app=\"python3 ~/.create.py\" >> ~/.bashrc
+
+source ~/.zshrc
+source ~/.bashrc
+
+
