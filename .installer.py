e6793c8dc2f6707552fd3fd8b3d36f783ba56b5d
danikhan632 <danikhan632@gmail.com>
2022-12-29 10:49:24 -0500

    112 commit

--- .installer.py
+++ .installer.py
@@ -1,8 +1,9 @@
 # sys.argv[1]
 import os
 import sys
+
 def main():
-    file1 = open("~/.zshrc", "a")  # append mode
+    file1 = open(os.environ.get('HOME'),"/.zshrc", "a")  # append mode
     file1.write("\n")
     file1.write("alias create-cpp-app=\"python3 ~/.create.py\"\n")
     file1.write("alias cpx=\"python3 scripts.py\"\n")
