3de8d975c47bb4562de3e4485232397d599be52a
danikhan632 <danikhan632@gmail.com>
2022-12-30 00:56:13 -0500

    120 commit

--- .create.py
+++ .create.py
@@ -3,9 +3,9 @@ import os
 import sys
 def main():
     os.system("git clone https://github.com/danikhan632/create-cpp-app.git")
-    os.system(" mv ./create-cpp-app ./", sys.argv[1])
-    os.system("cd ", sys.argv[1])
-    os.system("python3 scripts.py bootstrap ", sys.argv[1])    
+    os.system(str(" mv ./create-cpp-app ./"+ sys.argv[1]))
+    os.system(str("cd "+ sys.argv[1]))
+    os.system(str("python3 scripts.py bootstrap "+sys.argv[1]))    
 
 if __name__ == "__main__":
    main()
